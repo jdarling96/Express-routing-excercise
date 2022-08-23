e19e774e01be3e79e9c969c615d9a74640046437
joshdarling <joshua.a.darling@gmail.com>
2022-08-22 23:03:19 -0600

    all routes run there statistical operations

--- app.js
+++ app.js
@@ -8,12 +8,11 @@ app.get("/mean", (req, res, next) => {
   let count = 0;
   let query = req.query["nums"].split(",");
 
-  console.log(query);
   try {
     query.forEach((n, idx) => {
       if (n === "") throw new ExpressError("nums are required", 400);
       if (!Number(n)) throw new ExpressError(`${n} is not a number`, 400);
-      console.log(n);
+
       total += n;
       count++;
     });
@@ -26,14 +25,64 @@ app.get("/mean", (req, res, next) => {
   }
 });
 
+app.get("/median", (req, res, next) => {
+  let query = req.query["nums"].split(",");
+  let len = Math.floor(query.length / 2);
+  nums = query.sort((a, b) => a - b);
+  let value = 0;
 
-app.get('/median', (req, res, next) => {
-    
-})
+  try {
+    query.forEach((n, idx) => {
+      if (n === "") throw new ExpressError("nums are required", 400);
+      if (!Number(n)) throw new ExpressError(`${n} is not a number`, 400);
+      value =
+        query.length % 2 !== 0 ? query[len] : (query[len - 1] + query[len]) / 2;
+    });
+    return res.status(200).json({
+      operation: "mean",
+      value: Number(value),
+    });
+  } catch (e) {
+    return next(e);
+  }
+});
 
-app.get('/mode', (req, res, next) => {
-    
-})
+app.get("/mode", (req, res, next) => {
+  let query = req.query["nums"].split(",");
+  const obj = {};
+
+  try {
+    query.forEach((n) => {
+      if (n === "") throw new ExpressError("nums are required", 400);
+      if (!Number(n)) throw new ExpressError(`${n} is not a number`, 400);
+
+      if (!obj[n]) {
+        obj[n] = 1;
+      } else {
+        obj[n] += 1;
+      }
+    });
+
+    let highestVal = 0;
+    let highestValKey = -Infinity;
+
+    for (let key in obj) {
+      const value = obj[key];
+
+      if (value > highestVal) {
+        highestVal = value;
+        highestValKey = key;
+      }
+    }
+    return res.status(200).json({
+      operation: "mean",
+      value: Number(highestValKey),
+    });
+  } catch (e) {
+    return next(e);
+  }
+});
+
 app.use((error, req, res, next) => {
   let status = error.status || 500;
   let message = error.msg;
